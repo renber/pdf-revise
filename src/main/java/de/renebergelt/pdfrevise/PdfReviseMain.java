12bd20289783d507084f7beec32deffa1753acf0
renber <github@renebergelt.de>
2019-07-31 13:29:57 +0200

    Set version to 1.2.0(-SNAPSHOT)

--- src/main/java/de/renebergelt/pdfrevise/PdfReviseMain.java
+++ src/main/java/de/renebergelt/pdfrevise/PdfReviseMain.java
@@ -12,7 +12,7 @@ import java.util.*;
  */
 public class PdfReviseMain {
 
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
     public static final String COPYRIGHT = "Copyright 2019 Ren\u00E9 Bergelt";
 
     public static void main(String[] args) {
